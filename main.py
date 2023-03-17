82a51c6d3275b88767f4eb5f3dc6fcd67e5b3635
AdiP <84515343+Aditya-Prasad12343@users.noreply.github.com>
2023-03-17 13:22:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
 import streamlit as st
 import pandas as pd
 
-# Load the data
-data = pd.read_csv("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json")
-elements = data["elements"]
+# Load periodic table data
+elements = pd.read_csv("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json")['elements']
 
-# Define the columns to show
-columns = ["name", "symbol", "atomic_mass", "category", "appearance", "boil", "melt", "density", "discovered_by"]
+# Define element properties to show
+properties = ['name', 'symbol', 'atomic_mass', 'boiling_point', 'density', 'discovered_by']
 
-# Define the app layout
-st.set_page_config(page_title="Periodic Table", page_icon=":atom_symbol:", layout="wide")
-st.title("Periodic Table")
+# Create periodic table grid
+table = '<table style="border-collapse: collapse; border: 1px solid black;">'
+for row in range(1, 11):
+    table += '<tr>'
+    for col in range(1, 19):
+        element = elements[(row-1)*18 + col-1]
+        symbol = element['symbol']
+        name = element['name']
+        atomic_number = element['number']
+        table += f'<td style="border: 1px solid black; padding: 5px; text-align: center;"><a href="#{symbol}">{symbol}<br>{atomic_number}</a></td>'
+    table += '</tr>'
+table += '</table>'
 
-# Create a grid of buttons to display the elements
-col1, col2, col3, col4, col5, col6, col7 = st.beta_columns(7)
-for i in range(len(elements)):
-    element = elements[i]
-    button = None
-    if element["category"] == "diatomic nonmetal":
-        button = col1.button(element["symbol"], key=i)
-    elif element["category"] == "noble gas":
-        button = col2.button(element["symbol"], key=i)
-    elif element["category"] == "alkali metal":
-        button = col3.button(element["symbol"], key=i)
-    elif element["category"] == "alkaline earth metal":
-        button = col4.button(element["symbol"], key=i)
-    elif element["category"] == "metalloid":
-        button = col5.button(element["symbol"], key=i)
-    elif element["category"] == "halogen":
-        button = col6.button(element["symbol"], key=i)
-    else:
-        button = col7.button(element["symbol"], key=i)
+# Display periodic table
+st.markdown(table, unsafe_allow_html=True)
 
-    # Display the element information when button is clicked
-    if button:
-        st.write("## " + element["name"])
-        for col in columns:
-            if col in element:
-                st.write("**" + col.capitalize().replace("_", " ") + ":**", element[col])
+# Create element info section
+st.header("Element Info")
+
+# Handle element click event
+if st.session_state.clicked_element:
+    element = elements[st.session_state.clicked_element-1]
+    st.subheader(f"{element['name']} ({element['symbol']})")
+    for prop in properties:
+        if prop in element:
+            st.write(f"{prop.capitalize()}: {element[prop]}")
+else:
+    st.write("Click on an element to view its information.")
+
+# Add click event to table cells
+for element in elements:
+    symbol = element['symbol']
+    if st.button(symbol, key=symbol):
+        st.session_state.clicked_element = element['number']
