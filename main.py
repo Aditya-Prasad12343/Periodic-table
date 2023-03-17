a71c86de91f1db0d8e85be8314fc3b72a1494a91
AdiP <84515343+Aditya-Prasad12343@users.noreply.github.com>
2023-03-17 13:09:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+# Load the data
+data = pd.read_csv("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json")
+elements = data["elements"]
+
+# Define the columns to show
+columns = ["name", "symbol", "atomic_mass", "category", "appearance", "boil", "melt", "density", "discovered_by"]
+
+# Define the app layout
+st.set_page_config(page_title="Periodic Table", page_icon=":atom_symbol:", layout="wide")
+st.title("Periodic Table")
+
+# Create a grid of buttons to display the elements
+col1, col2, col3, col4, col5, col6, col7 = st.beta_columns(7)
+for i in range(len(elements)):
+    element = elements[i]
+    button = None
+    if element["category"] == "diatomic nonmetal":
+        button = col1.button(element["symbol"], key=i)
+    elif element["category"] == "noble gas":
+        button = col2.button(element["symbol"], key=i)
+    elif element["category"] == "alkali metal":
+        button = col3.button(element["symbol"], key=i)
+    elif element["category"] == "alkaline earth metal":
+        button = col4.button(element["symbol"], key=i)
+    elif element["category"] == "metalloid":
+        button = col5.button(element["symbol"], key=i)
+    elif element["category"] == "halogen":
+        button = col6.button(element["symbol"], key=i)
+    else:
+        button = col7.button(element["symbol"], key=i)
+
+    # Display the element information when button is clicked
+    if button:
+        st.write("## " + element["name"])
+        for col in columns:
+            if col in element:
+                st.write("**" + col.capitalize().replace("_", " ") + ":**", element[col])
