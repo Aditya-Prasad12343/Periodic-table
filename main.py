5d94134fb2140fe3ddeea437b03f1738fc09509f
AdiP <84515343+Aditya-Prasad12343@users.noreply.github.com>
2023-03-17 13:47:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,72 @@
 import streamlit as st
-import json
+import pandas as pd
 
-# Load the data
-with open("input.json", "r") as file:
-    data = json.load(file)
-    elements = data["elements"]
+# Load data
+elements = pd.read_csv('periodic_table.csv')
 
-# Define the columns to show
-columns = ["name", "symbol", "atomic_mass", "category", "appearance", "boil", "melt", "density", "discovered_by"]
+# Define layout
+table_style = """
+<style>
+table {
+  border-collapse: collapse;
+  margin: auto;
+}
+td {
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  border: 1px solid black;
+}
+.button {
+  background-color: #f2f2f2;
+  border: 1px solid #ccc;
+  color: black;
+  font-size: 16px;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+"""
+element_style = """
+<style>
+h1 {
+  text-align: center;
+}
+img {
+  display: block;
+  margin: auto;
+  width: 200px;
+}
+</style>
+"""
 
-# Define the app layout
-st.set_page_config(page_title="Periodic Table", page_icon=":atom_symbol:", layout="wide")
-st.title("Periodic Table")
-
-# Create a grid of buttons to display the elements
-col1, col2, col3, col4, col5, col6, col7 = st.beta_columns(7)
-for i in range(len(elements)):
-    element = elements[i]
-    button = None
-    if element["category"] == "diatomic nonmetal":
-        button = col1.button(element["symbol"], key=i)
-    elif element["category"] == "noble gas":
-        button = col2.button(element["symbol"], key=i)
-    elif element["category"] == "alkali metal":
-        button = col3.button(element["symbol"], key=i)
-    elif element["category"] == "alkaline earth metal":
-        button = col4.button(element["symbol"], key=i)
-    elif element["category"] == "metalloid":
-        button = col5.button(element["symbol"], key=i)
-    elif element["category"] == "halogen":
-        button = col6.button(element["symbol"], key=i)
+# Display periodic table
+st.markdown(table_style, unsafe_allow_html=True)
+table = "<table>"
+for i in range(1, 119):
+    element = elements[elements['atomic_number'] == i].iloc[0]
+    if element['symbol'] == '':
+        table += "<tr><td colspan='2'></td></tr>"
     else:
-        button = col7.button(element["symbol"], key=i)
+        button = f"<a href='#' class='button' onclick=\"element_key='{element['symbol']}'\">{element['symbol']}<br>{element['name']}</a>"
+        table += f"<tr><td>{button}</td><td>{element['atomic_number']}</td></tr>"
+table += "</table>"
+st.markdown(table, unsafe_allow_html=True)
 
-    # Display the element information when button is clicked
-    if button:
-        st.write("## " + element["name"])
-        for col in columns:
-            if col in element:
-                st.write("**" + col.capitalize().replace("_", " ") + ":**", element[col])
+# Display element information
+st.markdown(element_style, unsafe_allow_html=True)
+element_key = st.session_state.get('element_key')
+if element_key:
+    element = elements[elements['symbol'] == element_key].iloc[0]
+    st.header(f"{element['name']} ({element['symbol']})")
+    st.write(f"Atomic number: {element['atomic_number']}")
+    st.write(f"Atomic mass: {element['atomic_mass']}")
+    st.write(f"Period: {element['period']}")
+    st.write(f"Group: {element['group']}")
+    st.write(f"Electron configuration: {element['electron_configuration']}")
+    st.image(f"images/{element['symbol']}.png", width=200)
+
