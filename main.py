5b41cb1d9cdf53f7e5928965975a95155b323349
AdiP <84515343+Aditya-Prasad12343@users.noreply.github.com>
2023-03-17 13:56:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,41 @@
 import streamlit as st
-import pandas as pd
+import json
 
-# Load the periodic table data
-df = pd.read_csv('input.json')['elements']
-elements = {d['symbol']: d for d in df.to_dict('records')}
+# Load the data
+with open("input.json", "r") as file:
+    data = json.load(file)
+    elements = data["elements"]
 
-# Define the HTML template for the tooltip
-html_template = """
-<div style="background-color: #F4F4F4; padding: 10px;">
-    <h4 style="margin-top: 0px;">{name} ({symbol})</h4>
-    <p><b>Atomic number:</b> {number}</p>
-    <p><b>Atomic mass:</b> {atomic_mass}</p>
-    <p><b>Category:</b> {category}</p>
-    <p><b>Electron configuration:</b> {electron_configuration}</p>
-</div>
-"""
+# Define the columns to show
+columns = ["name", "symbol", "atomic_mass", "category", "appearance", "boil", "melt", "density", "discovered_by"]
 
-# Define the CSS style for the tooltip
-css_style = """
-<style>
-    /* Tooltip container */
-    .tooltip {
-        position: relative;
-        display: inline-block;
-        border-bottom: 1px dotted black;
-    }
+# Define the app layout
+st.set_page_config(page_title="Periodic Table", page_icon=":atom_symbol:", layout="wide")
+st.title("Periodic Table")
 
-    /* Tooltip text */
-    .tooltip .tooltiptext {
-        visibility: hidden;
-        width: 200px;
-        background-color: #F4F4F4;
-        color: black;
-        text-align: center;
-        border-radius: 6px;
-        padding: 10px;
-        position: absolute;
-        z-index: 1;
-        bottom: 125%;
-        left: 50%;
-        margin-left: -100px;
-        opacity: 0;
-        transition: opacity 0.3s;
-    }
+# Create a grid of buttons to display the elements
+col1, col2, col3, col4, col5, col6, col7 = st.beta_columns(7)
+for i in range(len(elements)):
+    element = elements[i]
+    button = None
+    if element["category"] == "diatomic nonmetal":
+        button = col1.button(element["symbol"], key=i)
+    elif element["category"] == "noble gas":
+        button = col2.button(element["symbol"], key=i)
+    elif element["category"] == "alkali metal":
+        button = col3.button(element["symbol"], key=i)
+    elif element["category"] == "alkaline earth metal":
+        button = col4.button(element["symbol"], key=i)
+    elif element["category"] == "metalloid":
+        button = col5.button(element["symbol"], key=i)
+    elif element["category"] == "halogen":
+        button = col6.button(element["symbol"], key=i)
+    else:
+        button = col7.button(element["symbol"], key=i)
 
-    /* Tooltip text arrow */
-    .tooltip .tooltiptext::after {
-        content: "";
-        position: absolute;
-        top: 100%;
-        left: 50%;
-        margin-left: -5px;
-        border-width: 5px;
-        border-style: solid;
-        border-color: black transparent transparent transparent;
-    }
-
-    /* Show the tooltip text when you mouse over the tooltip container */
-    .tooltip:hover .tooltiptext {
-        visibility: visible;
-        opacity: 1;
-    }
-</style>
-"""
-
-# Display the periodic table
-st.markdown(css_style, unsafe_allow_html=True)
-for row in range(1, 10):
-    for col in range(1, 19):
-        symbol = f'{col+((row-1)*18):03}'
-        element = elements.get(symbol, {})
-        if element:
-            tooltip_html = html_template.format(**element)
-            html = f'<div class="tooltip">{element["symbol"]}<span class="tooltiptext">{tooltip_html}</span></div>'
-            st.markdown(html, unsafe_allow_html=True)
-        else:
-            st.write('')
-
+    # Display the element information when button is clicked
+    if button:
+        st.write("## " + element["name"])
+        for col in columns:
+            if col in element:
+                st.write("**" + col.capitalize().replace("_", " ") + ":**", element[col])
